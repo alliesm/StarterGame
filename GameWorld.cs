bada2ce8887e5c2f488b4f86d495778c0da55d01
Brandon Barker <brandonjbarker12@gmail.com>
2021-05-01 19:48:31 -0400

    fixed gameworld

--- GameWorld.cs
+++ GameWorld.cs
@@ -101,53 +101,7 @@ namespace StarterGame
             return outside;
         }
 
-        /**
-     *  Main play routine.  Loops until end of play.
-     */
-        public void Play()
-        {
-
-            // Enter the main command loop.  Here we repeatedly read commands and
-            // execute them until the game is over.
-
-            bool finished = false;
-            while (!finished)
-            {
-                Console.Write("\n>");
-                Command command = parser.ParseCommand(Console.ReadLine());
-                if (command == null)
-                {
-                    Console.WriteLine("I don't understand...");
-                }
-                else
-                {
-                    finished = command.Execute(player);
-                }
-            }
-        }
-
-
-        public void Start()
-        {
-            playing = true;
-            player.OutputMessage(Welcome());
-        }
-
-        public void End()
-        {
-            playing = false;
-            player.OutputMessage(Goodbye());
-        }
-
-        public string Welcome()
-        {
-            return "Welcome to the World of CSU!\n\n The World of CSU is a new, incredibly boring adventure game.\n\nType 'help' if you need help." + player.CurrentRoom.Description();
-        }
-
-        public string Goodbye()
-        {
-            return "\nThank you for playing, Goodbye. \n";
-        }
+        
 
     }
 }
